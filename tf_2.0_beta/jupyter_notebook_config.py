cbd9b12eb68974244d1370199f50c2bb845252f4
Juan Trelles <juantrelles@gmail.com>
2019-07-16 21:29:54 -0700

    tf2 + jupyter

---
+++ tf_2.0_beta/jupyter_notebook_config.py
@@ -0,0 +1,5 @@
+c.NotebookApp.allow_remote_access = True
+c.NotebookApp.ip = '*'
+c.NotebookApp.password = u'sha1:031e9852ca0b:49418d1293afbb2a6446e07b726ad7c9796908e1'
+c.NotebookApp.port = 8888
+c.Notebook.open_browser = False
